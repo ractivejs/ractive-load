204ebfbbe813140975c601cec3a8ac49d06de586
marty <marty.nelson@yahoo.com>
2014-04-17 15:28:07 -0700

    use .js extension insteam of .html for better dev tool file display

--- src/rcu.amd.js
+++ src/rcu.amd.js
@@ -128,7 +128,7 @@ define( function() {
 				};
 				if ( definition.script ) {
 					try {
-						fn = new Function( 'component', 'require', 'Ractive', definition.script + '\n//# sourceURL=' + url.substr( url.lastIndexOf( '/' ) + 1 ) );
+						fn = new Function( 'component', 'require', 'Ractive', definition.script + '\n//# sourceURL=' + url.substr( url.lastIndexOf( '/' ) + 1 ).replace('.html','.js') );
 					} catch ( err ) {
 						errorMessage = 'Error creating function from component script: ' + err.message || err;
 						if ( onerror ) {
