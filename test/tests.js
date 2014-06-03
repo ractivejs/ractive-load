a56c25a1eb9e4c54eaaa2a9a278ec4cdcd71fd07
Rich Harris <richard.a.harris@gmail.com>
2014-06-03 08:07:56 -0400

    add tests, use mocha instead of qunit

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,57 @@
-// Ractive.load tests
-// ==================
-
 (function () {
 
-	var fixture;
+	'use strict';
 
-	test( 'We are ready to write some tests', function ( t ) {
-		t.ok( true );
+	var assert, load;
+
+	if ( typeof require === 'function' ) {
+		assert = require( 'assert' );
+		load = require( '../ractive-load' );
+		load.baseUrl = __dirname + '/';
+	} else {
+		assert = chai.assert;
+		load = Ractive.load;
+	}
+
+	describe( 'ractive-load', function () {
+		it( 'should load a simple component', function ( done ) {
+			var promise = load( 'sample/1/simple.html' ).then( function ( Component ) {
+				var ractive = new Component();
+				assert.equal( ractive.toHTML(), '<h1>Hello world!</h1>' );
+				done();
+			}).catch( done );
+		});
+
+		it( 'should load a nested component', function ( done ) {
+			var promise = load( 'sample/2/nested-outer.html' ).then( function ( Component ) {
+				var ractive = new Component();
+				assert.equal( ractive.toHTML(), '<div><p>this is a nested component</p></div>' );
+				done();
+			}).catch( done );
+		});
+
+		it( 'should load a nested component with relative paths', function ( done ) {
+			var promise = load( 'sample/3/nested-outer.html' ).then( function ( Component ) {
+				var ractive = new Component();
+				assert.equal( ractive.toHTML(), '<div><p>this is a nested component</p></div>' );
+				done();
+			}).catch( done );
+		});
+
+		it( 'should load multiple nested components with mix of relative-to-parent and relative-to-base paths', function ( done ) {
+			var promise = load( 'sample/4/nested-top.html' ).then( function ( Component ) {
+				var ractive = new Component();
+				assert.equal( ractive.toHTML(), '<div><div><p>this is a nested component</p></div></div>' );
+				done();
+			}).catch( done );
+		});
 	});
 
+	function throwError( err ) {
+		setTimeout( function () {
+			throw err;
+		});
+	}
 
 }());
+
